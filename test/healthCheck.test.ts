674294e50f47f41688ec4e685ded6b406c941096
harman kaur <harmankaur2@academic.rrc.ca>
2025-09-10 16:39:50 -0500

    Created the health check points.

---
+++ test/healthCheck.test.ts
@@ -0,0 +1,19 @@
+import request, { Response } from "supertest";
+import app from "../src/app";
+
+describe("GET /api/v1/health", () => {
+    it("health status should be 200", async () => {
+        const response: Response = await request(app).get("/api/v1/health");
+        expect(response.status).toBe(200);
+        expect(response.body.status).toBe("OK");
+    });
+    it("Positive uptime value", async() => {
+        const response: Response = await request(app).get("/api/v1/health");
+        expect(response.body.uptime).toBeGreaterThan(0);
+    });
+    it("Should have valid timestamp", async() => {
+        const response: Response = await request(app).get("/api/v1/health");
+        expect(response.body).toHaveProperty("timestamp");
+    });
+
+});
