8120d4d2d0b7a4bd6ecfc5f947ea89a90671cf18
harman kaur <harmankaur2@academic.rrc.ca>
2025-09-13 13:04:59 -0500

    Added the tests for portfolio performance.

--- jest.config.js
+++ jest.config.js
@@ -4,7 +4,7 @@ module.exports = {
     testMatch: ["**/*.test.ts"],
     collectCoverageFrom: [
         "src/**/*.ts",
-        "!src/server.ts", // Exclude server startup file
-        "!src/types/**/*.ts", // Exclude type definitions
+        "!src/server.ts", 
+        "!src/types/**/*.ts",
     ],
 };
